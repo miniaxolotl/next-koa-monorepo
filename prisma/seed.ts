92c02a831fa3fa837fd5951e383b0b569e42c77a
Elias <elias@emawa.io>
2022-02-04 10:04:33 -0700

    feature: initial migrations

---
+++ prisma/seed.ts
@@ -0,0 +1,73 @@
+import _ from 'lodash';
+
+import { Prisma } from '@prisma/client';
+
+import { RoleEnum } from '@libs/shared';
+import { ServerConfig } from '@libs/config';
+import { connectDB, db } from '@libs/database';
+import { createID, genHash } from '@libs/utility';
+
+(async () => {
+  await connectDB();
+
+  const roleData: Prisma.RoleCreateInput[] = _.map(RoleEnum, (r) => r);
+
+  const seed_password = async () => await genHash(ServerConfig.ADMIN_PASS);
+
+  const admin: Prisma.UserCreateInput = {
+    userId: createID(),
+    email: ServerConfig.ADMIN_EMAIL,
+    username: ServerConfig.ADMIN_USER,
+    password: await genHash(ServerConfig.ADMIN_PASS),
+  };
+
+  const userData: Promise<Prisma.UserCreateInput>[] = [
+    ..._.times(10, async () => ({
+      userId: createID(),
+      email: `${createID(6)}@emawa.io`,
+      username: `${createID(8)}`,
+      password: await seed_password(),
+    })),
+  ];
+
+  console.log('\n/**************** seeding ****************/');
+
+  console.log('\n---- role ----');
+
+  /************* ROLE *************/
+  for (const r of roleData) {
+    const role = await db.role.create({
+      data: r,
+    });
+    console.log(`created role: ${role.roleId}`);
+  }
+
+  console.log('\n---- user ----');
+
+  /************* USER *************/
+  const user = await db.user.create({
+    data: {
+      ...admin,
+      roles: {
+        create: [
+          { roleId: RoleEnum.DEVELOPER.roleId },
+          { roleId: RoleEnum.ADMIN.roleId },
+          { roleId: RoleEnum.USER.roleId },
+        ],
+      },
+    },
+  });
+  console.log(`created user:#${user.userId}\n${user.email}\n${user.password}\n`);
+
+  for (const u of userData) {
+    const user = await db.user.create({
+      data: {
+        ...(await u),
+        roles: {
+          create: [{ roleId: RoleEnum.USER.roleId }],
+        },
+      },
+    });
+    console.log(`created user:#${user.userId}\n${user.email}\n${user.password}\n`);
+  }
+})();
